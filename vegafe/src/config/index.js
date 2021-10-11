fefcac62fe198629b38d5757ae7111e0496b8971
khanh <duykhanh9242@gmail.com>
2021-10-10 22:51:26 -0400

    feat: add

---
+++ vegafe/src/config/index.js
@@ -0,0 +1,6 @@
+export const BASE_BSC_SCAN_URLS = {
+    56: 'https://bscscan.com',
+    97: 'https://testnet.bscscan.com',
+}
+
+export const BASE_BSC_SCAN_URL=BASE_BSC_SCAN_URLS[56]
---
+++ vegafe/src/utils/wallet.js
@@ -0,0 +1,35 @@
+import { BASE_BSC_SCAN_URL } from 'config'
+import { nodes } from './getRpcUrl'
+
+export const setupNetwork = async () => {
+    console.log('setupNetwork')
+    const provider = window.ethereum
+    if (provider) {
+        const chainId = parseInt(process.env.REACT_APP_CHAIN_ID, 10)
+        try {
+            await provider.request({
+                method: 'wallet_addEthereumChain',
+                params: [
+                    {
+                        chainId: `0x${chainId.toString(16)}`,
+                        chainName: 'Binance Smart Chain Mainnet',
+                        nativeCurrency: {
+                            name: 'BNB',
+                            symbol: 'bnb',
+                            decimals: 18,
+                        },
+                        rpcUrls: nodes,
+                        blockExplorerUrls: [`${BASE_BSC_SCAN_URL}/`],
+                    },
+                ],
+            })
+            return true
+        } catch (error) {
+            console.error('Failed to setup the network in Metamask:', error)
+            return false
+        }
+    } else {
+        console.error("Can't setup the BSC network on metamask because window.ethereum is undefined")
+        return false
+    }
+}

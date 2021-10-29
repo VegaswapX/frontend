672b63e84c92f1d16f94cbe2b410232862b9311b
benjyz <ben@vegaswap.io>
2021-10-29 17:15:32 +0700

    pool1

--- client/src/chain/Contracts.js
+++ client/src/chain/Contracts.js
@@ -1,5 +1,6 @@
 //MAINNET
-export const POOL1 = "0x3f8a4C7AA9265eDCA2272197D902a8992698C3A5";
+//export const POOL1 = "0x3f8a4C7AA9265eDCA2272197D902a8992698C3A5";
+export const POOL1 = "0x66887872e5dfc545038944ed71ff154430a13c03"
 export const POOL2 = "";
 export const VEGA_TOKEN_ADDRESS = "0x4EfDFe8fFAfF109451Fc306e0B529B088597dd8d";
 export const BSC_USDT = "0x55d398326f99059fF775485246999027B3197955";
@@ -13,6 +14,15 @@ export const PCS_ROUTER_ADDRESS = "0x10ed43c718714eb63d5aa57b78b54704e256024e";
 //export const POOL1 = "0xE7eD6747FaC5360f88a2EFC03E00d25789F69291";
 
 export const BPOOLS = [
+  // {
+  //   poolName: "USDT-VGA",
+  //   address: POOL1,
+  //   description:
+  //     "Stake USDT and earn VGA. The staked amount will be locked for the specified time. After the expiration the USDT amount will be returned together with the reward in VGA",
+  //   per: "VGA per USDT",
+  //   stakedUnit: "USDT",
+  //   yieldUnit: "VGA"
+  // },
   {
     poolName: "USDT-VGA",
     address: POOL1,

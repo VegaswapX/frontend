c45e7af2770a45c7efe1c67ca7df3516ec117d49
benjyz <ben@vegaswap.io>
2021-10-31 12:58:56 +0700

    cg api

---
+++ client/src/api/data.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const CG_BASE = "https://api.coingecko.com/api/v3/simple/";
+const URL = CG_BASE + "price?ids=binancecoin,vegaswap&vs_currencies=usd";
+
+function getPrices() {
+    return new Promise(resolve => {
+      let res = axios.get(URL);
+      if (res){
+        resolve(res);  
+      }
+    //   setTimeout(() => {
+    //     resolve('resolved');
+    //   }, 2000);
+    });
+  }
+
+export async function agetPrices() {
+  console.log('calling');
+  const result = await getPrices();
+  if (result) {
+      console.log(result.data["binancecoin"]);
+  }
+}

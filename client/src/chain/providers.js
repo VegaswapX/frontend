a6eba11d8fc784589f6ff82c38b6a0686a36a1bb
rhacker <nxqd.inbox+rhacker@gmail.com>
2021-11-04 00:28:29 +0700

    add: providers

---
+++ client/src/chain/providers.js
@@ -0,0 +1,24 @@
+import {ethers} from "ethers";
+
+export const nodes = [
+  "https://bsc-dataseed1.ninicoin.io",
+  "https://bsc-dataseed1.defibit.io",
+  "https://bsc-dataseed.binance.org",
+];
+
+const getRandomIndex = () => {
+  const lower = 0;
+  const upper = nodes.length - 1;
+  return Math.floor(lower + Math.random() * (upper - lower + 1));
+};
+
+const getNodeUrl = (): string => {
+  const randomIndex = getRandomIndex();
+  return nodes[randomIndex];
+};
+
+const RPC_URL = getNodeUrl();
+
+const simpleRpcProvider = new ethers.providers.JsonRpcProvider(RPC_URL);
+
+export default simpleRpcProvider;

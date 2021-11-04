33910f00dd31add0a001411e0e9fe2fc39d1f910
VCryptoboy <tjohnny600@gmail.com>
2021-11-04 22:40:34 +0700

    add fontawesome, improve UI

--- client/src/pages/Swap/CurrencySelect.js
+++ client/src/pages/Swap/CurrencySelect.js
@@ -3,29 +3,8 @@ import { Button, Modal } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { store } from "../../redux/store";
 import Tokentable from "./tokentable.js";
-
-function CurrencyButton(props) {
-  return (
-    <Button
-      size="lg"
-      style={{
-        backgroundColor: "#1f2125",
-        border: "3px solid #333",
-        borderRadius: "8px",
-        height: "50px",
-        width: "140px",
-        boxShadow: "none",
-      }}
-      onClick={props.toggle}
-    >
-      <img
-        src={props.currency.image}
-        style={{ width: "24px", height: "24px", marginRight: "10px" }}
-      />
-      {props.currency.symbol}
-    </Button>
-  );
-}
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 
 function toggle(tokenIndex) {
   return () => {
@@ -42,33 +21,33 @@ export function CurrencySelectorModal({}) {
   const [className] = useState(null);
   const [scroll] = useState(null);
   const isTokenSelectorModalOpened = useSelector(
-    (state) => state.uiReducer.isTokenSelectorModalOpened,
+      (state) => state.uiReducer.isTokenSelectorModalOpened,
   );
   const currentModalTokenIndex = useSelector(
-    (state) => state.uiReducer.currentModalTokenIndex,
+      (state) => state.uiReducer.currentModalTokenIndex,
   );
 
   return (
-    <Modal
-      show={isTokenSelectorModalOpened}
-      onHide={toggle()}
-      dialogClassName={className}
-      size={size}
-      scrollable={scroll}
-    >
-      <Modal.Header onHide={toggle()} closeButton>
-        <h4 className="modal-title">Select token</h4>
-      </Modal.Header>
-      <Modal.Body>
-        <Tokentable tokenIndex={currentModalTokenIndex} />
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="light" onClick={toggle()}>
-          Close
-        </Button>
-        {" "}
-      </Modal.Footer>
-    </Modal>
+      <Modal
+          show={isTokenSelectorModalOpened}
+          onHide={toggle()}
+          dialogClassName={className}
+          size={size}
+          scrollable={scroll}
+      >
+        <Modal.Header onHide={toggle()} closeButton>
+          <h4 className="modal-title">Select token</h4>
+        </Modal.Header>
+        <Modal.Body>
+          <Tokentable tokenIndex={currentModalTokenIndex} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="light" onClick={toggle()}>
+            Close
+          </Button>
+          {" "}
+        </Modal.Footer>
+      </Modal>
   );
 }
 
@@ -78,8 +57,30 @@ export function CurrencySelector({ token, tokenIndex }) {
   // console.log(`res`, res);
   // console.log(`toggleHandle`, toggleHandle);
   return (
-    <span>
-      <CurrencyButton currency={token} toggle={toggleHandle} />
-    </span>
+      <Button
+          size="lg"
+          style={{
+            background: "#22262c",
+            // border: "3px solid #333",
+            // borderRadius: "8px",
+            height: "50px",
+            width: "140px",
+            boxShadow: "none",
+            border: "none",
+            fontSize: "1rem",
+          }}
+          onClick={toggleHandle}
+      >
+        <img
+            src={token.image}
+            style={{ width: "24px", height: "24px", marginRight: "10px" }}
+        />
+        {token.symbol}
+        <FontAwesomeIcon
+            style={{
+              marginLeft: "8px"
+            }}
+            icon={faCaretDown} />
+      </Button>
   );
 }

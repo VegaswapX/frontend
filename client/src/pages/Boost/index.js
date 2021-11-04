244c79223024c194a91f0877485bd19b035aa8d9
benjyz <ben@vegaswap.io>
2021-11-04 14:25:39 +0700

    improve pool overview

--- client/src/pages/Boost/index.js
+++ client/src/pages/Boost/index.js
@@ -10,18 +10,68 @@ import { store } from "../../redux/store";
 import { InfoModal } from "./InfoModal";
 import { StakeModal } from "./StakeModal";
 
+const VGA_IMG =
+  "https://assets.coingecko.com/coins/images/18397/small/big_logo.png?1631769696";
+const USDT_IMG =
+  "https://assets.coingecko.com/coins/images/325/small/Tether-logo.png?1598003707";
+
+
 const PoolRow = ({ pool }) => {
   console.log(">>> " + pool.poolName);
 
   return (
-    <tr>
-      <td>
+    <tr style={{textAlign: "center", color: "white", cellpadding: "10px"}}>
+       <td>
         {pool.poolName}&nbsp;&nbsp;
-        <InfoModal pool={pool} />
+      </td>
+
+      <td>
+      <img src={`${USDT_IMG}`} alt="currency"></img>
+      </td>
+
+      <td>
+      <img src={`${VGA_IMG}`} alt="currency"></img>
+      </td>      
+
+      <td>
+        <StakeModal pool={pool} />        
       </td>
       <td>
-        <StakeModal pool={pool} />
+      <InfoModal pool={pool} />
       </td>
+      {/* <td>
+        TVL
+      </td> */}
+    </tr>
+  );
+};
+
+const HeadRow = ({ pool }) => {
+
+  return (
+    <tr style={{textAlign: "center", color: "white", cellpadding: "10px"}}>
+       <td>
+        Poolname
+      </td>
+
+      <td>
+      Stake currency
+      </td>
+
+      <td>
+      Yield currency
+      </td>     
+
+      <td>
+        Stake
+      </td>
+      <td>
+      Info
+      </td>
+
+      {/* <td>
+        TVL
+      </td> */}
     </tr>
   );
 };
@@ -29,7 +79,9 @@ const PoolRow = ({ pool }) => {
 const PoolsList = () => {
   return (
     <>
-      <Table className="mb-0" style={{ width: "40%", border: "0px" }}>
+      {/* <Table className="mb-0" style={{ width: "40%", border: "solid white 1px;", cellspacing: "10px", backgroundColor: "#1c1f27" }}> */}
+      <Table style={{ width: "40%", border: "solid white 1px;", cellspacing: "10px", backgroundColor: "#1c1f27" }}>
+        <HeadRow />
         {BPOOLS.map((pool) => {
           return <PoolRow pool={pool} />;
         })}

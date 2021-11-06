b3dc49ca828ae30bc435ae38d304343b5d6c42e8
VCryptoboy <tjohnny600@gmail.com>
2021-11-06 16:47:04 +0700

    add lightweight charts, opesource, license free

--- client/src/components/TVComponent/index.js
+++ client/src/components/TVComponent/index.js
@@ -1,86 +1,36 @@
-// import * as React from 'react';
-// import './index.css';
-// import { widget } from '../../charting_library';
-//
-// function getLanguageFromURL() {
-//   const regex = new RegExp('[\\?&]lang=([^&#]*)');
-//   const results = regex.exec(window.location.search);
-//   return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, ' '));
-// }
-//
-// export class TVChartContainer extends React.PureComponent {
-//   static defaultProps = {
-//     symbol: 'AAPL',
-//     interval: 'D',
-//     containerId: 'tv_chart_container',
-//     datafeedUrl: 'https://demo_feed.tradingview.com',
-//     libraryPath: '/charting_library/',
-//     chartsStorageUrl: 'https://saveload.tradingview.com',
-//     chartsStorageApiVersion: '1.1',
-//     clientId: 'tradingview.com',
-//     userId: 'public_user_id',
-//     fullscreen: false,
-//     autosize: true,
-//     studiesOverrides: {},
-//   };
-//
-//   tvWidget = null;
-//
-//   componentDidMount() {
-//     const widgetOptions = {
-//       symbol: this.props.symbol,
-//       // BEWARE: no trailing slash is expected in feed URL
-//       datafeed: new window.Datafeeds.UDFCompatibleDatafeed(this.props.datafeedUrl),
-//       interval: this.props.interval,
-//       container_id: this.props.containerId,
-//       library_path: this.props.libraryPath,
-//
-//       locale: getLanguageFromURL() || 'en',
-//       disabled_features: ['use_localstorage_for_settings'],
-//       enabled_features: ['study_templates'],
-//       charts_storage_url: this.props.chartsStorageUrl,
-//       charts_storage_api_version: this.props.chartsStorageApiVersion,
-//       client_id: this.props.clientId,
-//       user_id: this.props.userId,
-//       fullscreen: this.props.fullscreen,
-//       autosize: this.props.autosize,
-//       studies_overrides: this.props.studiesOverrides,
-//     };
-//
-//     const tvWidget = new widget(widgetOptions);
-//     this.tvWidget = tvWidget;
-//
-//     tvWidget.onChartReady(() => {
-//       tvWidget.headerReady().then(() => {
-//         const button = tvWidget.createButton();
-//         button.setAttribute('title', 'Click to show a notification popup');
-//         button.classList.add('apply-common-tooltip');
-//         button.addEventListener('click', () => tvWidget.showNoticeDialog({
-//           title: 'Notification',
-//           body: 'TradingView Charting Library API works correctly',
-//           callback: () => {
-//             console.log('Noticed!');
-//           },
-//         }));
-//
-//         button.innerHTML = 'Check API';
-//       });
-//     });
-//   }
-//
-//   componentWillUnmount() {
-//     if (this.tvWidget !== null) {
-//       this.tvWidget.remove();
-//       this.tvWidget = null;
-//     }
-//   }
-//
-//   render() {
-//     return (
-//         <div
-//             id={ this.props.containerId }
-//             className={ 'TVChartContainer' }
-//         />
-//     );
-//   }
-// }
+import { createChart } from "lightweight-charts";
+import React from "react";
+
+class ChartWrapper extends React.Component {
+  state = {};
+
+  constructor(props) {
+    super(props);
+    this.chartDiv = React.createRef();
+  }
+
+  componentDidMount() {
+    this.chart = createChart(this.chartDiv.current, {
+      width: 800,
+      height: 400,
+      timeScale: {
+        timeVisible: true,
+        secondsVisible: false,
+      },
+      watermark: {
+        visible: true,
+        fontSize: 34,
+        color: "rgba(0, 0, 0, 0.25)",
+      },
+    });
+  }
+
+  render() {
+    return (
+      <div ref={this.chartDiv} style={{ position: "relative" }}>
+      </div>
+    );
+  }
+}
+
+export default ChartWrapper;

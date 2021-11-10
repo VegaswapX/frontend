4078e75801f48ce9b705909f664cb8c02aaf32ea
VCryptoboy <tjohnny600@gmail.com>
2021-11-10 22:12:06 +0700

    add data from bitquery

--- client/src/components/TVComponent/index.js
+++ client/src/components/TVComponent/index.js
@@ -1,35 +1,40 @@
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 import { createChart } from "lightweight-charts";
 import React, { useEffect, useRef, useState } from "react";
-import { exampleData } from "./data";
+import { exampleData, minutesData } from "./data";
 const BITQUERY_ENDPOINT = `https://graphql.bitquery.io`;
 const graphqlCache = new InMemoryCache();
 
 const exampleQuery = gql `
-{
-  ethereum(network: bsc) {
-    dexTrades(options: {limit: 100, asc: "timeInterval.minute"}, date: {since: "2020-11-01"}, exchangeName: {in: ["Pancake", "Pancake v2"]}, baseCurrency: {is: "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"}, quoteCurrency: {is: "0xe9e7cea3dedca5984780bafc599bd69add087d56"}) {
-      timeInterval {
-        minute(count: 5)
+query getOHLC($minuteInterval: Int, $baseCurrency: String, $quoteCurrency: String) {
+    ethereum(network: bsc) {
+      dexTrades(options: {
+        limit: 100, 
+        asc: "timeInterval.minute"}, 
+        date: {since: "2020-11-01"}, 
+        exchangeName: {in: ["Pancake", "Pancake v2"]}, 
+        baseCurrency: {is: $baseCurrency}, quoteCurrency: {is: $quoteCurrency}) {
+        timeInterval {
+          minute(count: $minuteInterval)
+        }
+        baseCurrency {
+          symbol
+          address
+        }
+        baseAmount
+        quoteCurrency {
+          symbol
+          address
+        }
+        quoteAmount
+        trades: count
+        quotePrice
+        maximum_price: quotePrice(calculate: maximum)
+        minimum_price: quotePrice(calculate: minimum)
+        open_price: minimum(of: block, get: quote_price)
+        close_price: maximum(of: block, get: quote_price)
       }
-      baseCurrency {
-        symbol
-        address
-      }
-      baseAmount
-      quoteCurrency {
-        symbol
-        address
-      }
-      quoteAmount
-      trades: count
-      quotePrice
-      maximum_price: quotePrice(calculate: maximum)
-      minimum_price: quotePrice(calculate: minimum)
-      open_price: minimum(of: block, get: quote_price)
-      close_price: maximum(of: block, get: quote_price)
     }
-  }
 }
 `;
 
@@ -52,29 +57,42 @@ function transformToChartData(graphqlData) {
 }
 
 // TODO: Measure response time
-try {
-  client.query({
-    query: exampleQuery,
-  }).then(res => {
+async function getOHLCData(minuteInterval = 1440, baseCurrency, quoteCurrency) {
+  try {
+    const t0 = performance.now();
+    const res = await client.query({
+      query: exampleQuery,
+      variables: {
+        "minuteInterval": minuteInterval,
+        "baseCurrency": "0x4EfDFe8fFAfF109451Fc306e0B529B088597dd8d",
+        "quoteCurrency": "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+      },
+    });
+    const t1 = performance.now();
+    // DEBUG
+    console.log(`[bitquery] responseTime`, t1 - t0);
+
     console.log("graphql res", res);
     const { dexTrades } = res?.data?.ethereum;
     const chartData = transformToChartData(dexTrades);
-    console.log(`chartData`, chartData);
-  });
-} catch (e) {
-  console.log(`e`, e);
+    return chartData;
+    // console.log(`chartData`, chartData);
+  } catch (e) {
+    console.log(`e`, e);
+  }
 }
 
 export function ChartWrapper() {
   const chartDiv = useRef();
   const [chart, setChart] = useState(null);
 
-  useEffect(() => {
+  useEffect(async () => {
     const chart_ = createChart(chartDiv.current, {
       width: 800,
       height: 400,
 
       timeScale: {
+        autoScale: true,
         timeVisible: true,
         secondsVisible: false,
       },
@@ -86,12 +104,17 @@ export function ChartWrapper() {
       },
     });
 
-    const priceData = exampleData;
+    // timeScale
+    // const timeScale = chart_.timeScale().options();
+    // console.log(`timeScale`, timeScale);
+    const lineSeries = chart_.addCandlestickSeries();
+    setChart(chart_);
+
+    const priceData = await getOHLCData();
+    console.log(`priceData`, priceData);
 
     // candle chart
-    const lineSeries = chart_.addCandlestickSeries();
     lineSeries.setData(priceData);
-    setChart(chart_);
   }, []);
 
   return (

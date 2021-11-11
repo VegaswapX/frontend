39bcceeeac864340a26e01d1018da1ce2234e362
VCryptoboy <tjohnny600@gmail.com>
2021-11-11 21:27:50 +0700

    fix query bug

--- client/src/components/TVComponent/index.js
+++ client/src/components/TVComponent/index.js
@@ -37,11 +37,11 @@ query getOHLC($minuteInterval: Int, $baseCurrency: String, $quoteCurrency: Strin
         close_price: maximum(of: block, get: quote_price)
       }
       BNBUSDT: dexTrades(
-      options: {limit: 100, asc: "timeInterval.minute"}
-      date: {since: "2020-11-01"}
-      exchangeName: {in: ["Pancake", "Pancake v2"]}
-      baseCurrency: {is: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"}
-      quoteCurrency: {is: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"}
+        options: {limit: 100, asc: "timeInterval.minute"}
+        date: {since: "2020-11-01"}
+        exchangeName: {in: ["Pancake", "Pancake v2"]}
+        baseCurrency: {is: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"}
+        quoteCurrency: {is: "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"}
       ) {
         timeInterval {
           minute(count: $minuteInterval)
@@ -63,7 +63,6 @@ query getOHLC($minuteInterval: Int, $baseCurrency: String, $quoteCurrency: Strin
         open_price: minimum(of: block, get: quote_price)
         close_price: maximum(of: block, get: quote_price)
       }
-    }
     }
 }
 `;
